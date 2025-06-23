926728fb4e559250a039c5c292fb6a658badc884
Sooah Lee <sooahlee01@gmail.com>
2025-06-23 11:06:31 +0900

    Add ui for planning toggle (#30) - Change name from usePlanning into usePhasePlanning (#31) - Add variables related to planning into taskState

--- src/core/task/index.ts
+++ src/core/task/index.ts
@@ -355,6 +355,7 @@ export class Task {
 			this.workspaceTracker,
 			this.contextManager,
 			this.cacheService,
+			this.sidebarController,
 			this.autoApprovalSettings,
 			this.browserSettings,
 			cwd,
@@ -414,7 +415,7 @@ export class Task {
 		}
 
 		// Build and return the temporary API handler
-		return buildApiHandler(tempConfig, this.chatSettings.mode)
+		return buildApiHandler(tempConfig, this.mode)
 	}
 
 	async restoreCheckpoint(messageTs: number, restoreType: ClineCheckpointRestore, offset?: number) {
@@ -1078,16 +1079,6 @@ export class Task {
 
 		await this.postStateToWebview()
 
-		let phaseAwarePrompt: string
-		phaseAwarePrompt =
-			this.phaseTracker && !this.isPhaseRoot
-				? buildPhasePrompt(
-						this.phaseTracker.currentPhase,
-						this.phaseTracker.totalPhases,
-						this.phaseTracker.getProjectOverview(),
-					)
-				: (task ?? "")
-
 		await this.say("text", task, images, files)
 		this.taskState.isInitialized = true
 
@@ -1164,11 +1155,25 @@ export class Task {
 
 		let imageBlocks: Anthropic.ImageBlockParam[] = formatResponse.imageBlocks(images)
 		let userContent: UserContent = []
-		if (this.isPhaseRoot) {
-			userContent = [{ type: "text", text: `${PROMPTS.PLANNING}\n\n<task>\n${finalTask}\n</task>` }, ...imageBlocks]
+		let phaseAwarePrompt: string = ""
+		if (this.autoApprovalSettings.actions.usePhasePlanning) {
+			phaseAwarePrompt =
+				this.phaseTracker && !this.isPhaseRoot
+					? buildPhasePrompt(
+							this.phaseTracker.currentPhase,
+							this.phaseTracker.totalPhases,
+							this.phaseTracker.getProjectOverview(),
+						)
+					: (task ?? "")
+			if (this.isPhaseRoot) {
+				userContent = [{ type: "text", text: `${PROMPTS.PLANNING}\n\n<task>\n${finalTask}\n</task>` }, ...imageBlocks]
+			} else {
+				userContent = [{ type: "text", text: `<task>\n${phaseAwarePrompt}\n</task>` }, ...imageBlocks]
+			}
 		} else {
-			userContent = [{ type: "text", text: `<task>\n${phaseAwarePrompt}\n</task>` }, ...imageBlocks]
+			userContent = [{ type: "text", text: `<task>\n${task}\n</task>` }, ...imageBlocks]
 		}
+
 		if (files?.length) {
 			const fileContentString = await processFilesIntoText(files)
 			if (fileContentString) {
@@ -1179,14 +1184,18 @@ export class Task {
 			}
 		}
 
-		// Planning Phase
-		if (this.isPhaseRoot) {
-			await this.executePlanningPhase(userContent)
-			// await this.executePlanningPhase(phaseAwarePrompt)
-		}
+		if (this.autoApprovalSettings.actions.usePhasePlanning) {
+			// Planning Phase
+			if (this.isPhaseRoot) {
+				await this.executePlanningPhase(userContent)
+				// await this.executePlanningPhase(phaseAwarePrompt)
+			}
 
-		// Execution Phase
-		await this.executeCurrentPhase()
+			// Execution Phase
+			await this.executeCurrentPhase()
+		} else {
+			await this.initiateTaskLoop(userContent)
+		}
 	}
 
 	private async executePlanningPhase(userBlocks: UserContent): Promise<void> {
@@ -1218,7 +1227,7 @@ export class Task {
 
 		// Mark the first phase as complete
 		this.phaseTracker.markCurrentPhaseComplete()
-		this.sidebarController.onPhaseCompleted(this)
+		this.sidebarController.onPhaseCompleted()
 
 		// Start execution of the first phase
 		this.newPhaseOpened = false
@@ -1254,7 +1263,8 @@ export class Task {
 
 		const userBlocks: UserContent = [{ type: "text", text: `<task>\n${currentPhasePrompt}\n</task>` }]
 
-		const phaseFinished = await this.initiateTaskLoop(userBlocks)
+		const phaseFinished = (await this.initiateTaskLoop(userBlocks)) || false
+		this.taskState.phaseFinished = phaseFinished
 		if (phaseFinished) {
 			this.sidebarController.onTaskCompleted()
 		}

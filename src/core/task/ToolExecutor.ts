8de085ba4fce0bc727de421adbc9fd66a3c28364
Sooah Lee <sooahlee01@gmail.com>
2025-07-03 09:35:28 +0900

    Save planning result in md file (#58)
    * feature: save planning result in md file
    - Save md file and snapshot md file
    - Fix init phasetracker in controller
    - Add parsing plan from plan md
    - Update phasetracker from parsed result
    - Enhance text for planning approval
    - Separte ask_proceed and ask_check for askUserApproval
    - Improve webview-ui for planning
    * fix: save md file with taskId
    - Update phaseTracker even if diff is not existed
    
    fix: turn on usercontent (#59)

--- src/core/task/ToolExecutor.ts
+++ src/core/task/ToolExecutor.ts
@@ -2368,7 +2368,7 @@ export class ToolExecutor {
 								await this.saveCheckpoint()
 							} else {
 								const result = await this.sidebarController.task?.askUserApproval(
-									"ask_question",
+									"ask_proceed",
 									PROMPTS.MOVE_NEXT_PHASE_ASK,
 								)
 								if (!result) {
